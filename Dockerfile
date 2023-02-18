3f01bde9b1536afa1609e347622e6c151dfd1c06
mohamad mokhtar mokhtarzadeh tabrizi <m.mokhtarzadeh@hotmail.com>
2023-02-19 00:51:34 +0330

    - Add source file for using kubectl easier

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM alpine:3.17.2
+COPY source.sh /root/source.sh
 RUN apk update && \
 apk add curl && \
 curl -L -O "https://dl.k8s.io/release/v1.23.15/bin/linux/amd64/kubectl" && \
---
+++ source.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+API_SERVER="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT"
+CA_CRT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+alias k='kubectl --server="$API_SERVER" --certificate-authority="$CA_CRT" --token="$TOKEN"'
